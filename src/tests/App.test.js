d7273bda8f4ed7b400e01a210bd6d4249458f2b1
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-05-11 10:55:09 -0300

    Requsito 01 limpando o código

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -8,8 +8,11 @@ describe('1. Teste o componente <App.js />', () => {
   test('O primeiro link deve possuir o texto Home', () => {
     const { history } = renderWithRouter(<App />);
 
+    // verifica se o link com o texto 'Home' está na tela
     const linkHome = screen.getByRole('link', { name: /Home/i });
+    // clica no link 'Home'
     userEvent.click(linkHome);
+    // verifica se o link 'Home' tem o "pathname"(rota) correto
     const { pathname } = history.location;
     expect(pathname).toBe('/');
   });
@@ -31,49 +34,4 @@ describe('1. Teste o componente <App.js />', () => {
     const { pathname } = history.location;
     expect(pathname).toBe('/favorites');
   });
-  //
-
-  // expect(linkAbout).toBeInTheDocument();
-
-  //
-  // expect(linkFavoritePokemons).toBeInTheDocument();
-
-  // test('Teste ao clicar no link Home redireciona para a página inicial', () => {
-  //   const { history } = renderWithRouter(<App />);
-
-  //   const linkHome = screen.getByRole('link', { name: /Home/i });
-  //   expect(linkHome).toBeInTheDocument();
-
-  //
-  //   console.log(pathname);
-  // });
-
-  // test('Teste se a aplicação é redirecionada para a página de About', () => {
-  //   const { history } = renderWithRouter(<App />);
-
-  //   const linkAbout = screen.getByRole('link', { name: /About/i });
-  //   expect(linkAbout).toBeInTheDocument();
-
-  //   userEvent.click(linkAbout);
-  //   const { pathname } = history.location;
-  //   expect(pathname).toBe('/about');
-  // });
-
-  // test('Teste se a aplicação é redirecionada para a página de About', () => {
-  //   const { history } = renderWithRouter(<App />);
-
-  //   const linkFavoritePokemons = screen.getByRole('link', { name: /Favorite Pokémons/i });
-  //   expect(linkFavoritePokemons).toBeInTheDocument();
-
-  //   userEvent.click(linkFavoritePokemons);
-  //   const { pathname } = history.location;
-  //   expect(pathname).toBe('/fovorites');
-  // });
-
-  // test('Teste se a aplicação é redirecionada para a página Not Found ', () => {
-  //   renderWithRouter(<App />);
-  //   const notFound = screen
-  //     .getByText('Page requested not found 😭');
-  //   expect(notFound).toBeInTheDocument();
-  // });
 });
