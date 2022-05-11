76a1d6ed92a47b76ce551f741150fdb983732266
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-05-11 11:09:44 -0300

    Requsito 01 limpando o código2.1

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
-import renderWithRouter from '../components/renderWithRouter';
-import App from '../App';
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import renderWithRouter from '../components/renderWithRouter';
+// import App from '../App';
 
 describe('1. Teste o componente <FavoritePokemons.js />', () => {
-  test('Testa se é exibida na tela a mensagem', () => {
-    renderWithRouter(<App />);
 
-    const message = screen.getByText('No favorite pokemon found');
-    expect(message).toBeInTheDocument();
-  });
 });
