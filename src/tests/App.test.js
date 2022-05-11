c513bd22709199d506f8967001233b74dee699f9
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-05-11 11:54:56 -0300

    Requsito 01 limpando o código2.1.1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouter from '../components/renderWithRouter';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 describe('1. Teste o componente <App.js />', () => {
