e1a400e4c1f0fda8e7062a398e99b22f41aa5d9f
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-05-13 12:06:47 -0300

    Requisito 03 FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,8 +1,19 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// import renderWithRouter from '../components/renderWithRouter';
-// import App from '../App';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
 
 describe('1. Teste o componente <FavoritePokemons.js />', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const favorite = screen.getByText(/No favorite pokemon found/i);
+    expect(favorite).toBeInTheDocument();
+  });
 
+  test('Teste se são exibidos todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    // const favorites = container.querySelector('#root > div > div > div > div > div');
+    // expect(favorites).toBeInTheDocument();
+  });
 });
